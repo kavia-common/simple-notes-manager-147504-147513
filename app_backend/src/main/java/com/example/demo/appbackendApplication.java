52fd200104400695de46c2ec4a237f1574e5c76f
Kavia AI <production@kavia.ai>
2025-10-07 16:30:41 +0000

    CheckPoint - cg92a0478c

---
+++ app_backend/src/main/java/com/example/demo/appbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.appbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class appbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(appbackendApplication.class, args);
+	}
+
+}
